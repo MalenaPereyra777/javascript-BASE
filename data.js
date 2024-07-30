c83aea9734e1eeb3a38964816c4ed0cd1d0b639c
MalenaPereyra777 <pereyramalena15@gmail.com>
2024-07-30 01:15:13 -0300

    Base de trabajo

---
+++ data.js
@@ -0,0 +1,99 @@
+
+const productsData = [
+ {
+    id: 1,
+    name: "Emilia",
+    price: 10.500,
+    category: "Sets",
+ },
+ {
+    id: 2,
+    name: "Madison",
+    price: 12.000,
+    category: "Sets",
+}, 
+{
+    id: 3,
+    name: "Rosina",
+    price: 13.500,
+    category: "Sets",
+},
+{
+    id: 4,
+    name: "Santorini",
+    price: 15.500,
+    category: "Sets",
+},
+{
+    id: 5,
+    name: "Cleo",
+    price: 9.900,
+    category: "SS24",
+},
+{
+    id: 6,
+    name: "Moana",
+    price: 10.000,
+    category: "SS24",
+},
+{
+    id: 7,
+    name: "Eva",
+    price: 10.500,
+    category: "SS24",
+},
+{
+    id: 8,
+    name: "Danna",
+    price: 11.000,
+    category: "SS24",
+},
+{
+    id: 9,
+    name: "Cassie",
+    price: 8.000,
+    category: "Vestidos",
+},
+{
+    id: 10,
+    name: "Kylie",
+    price: 8.800,
+    category: "Vestidos",
+},
+{
+    id: 11,
+    name: "Kendall",
+    price: 9.000,
+    category: "Vestidos",
+},
+{
+    id: 12,
+    name: "Lena",
+    price: 9.500,
+    category: "Vestidos",
+},
+{
+    id: 13,
+    name: "Ambar",
+    price: 4.500,
+    category: "Tops",
+},
+{
+    id: 14,
+    name: "Emma",
+    price: 5.000,
+    category: "Tops",
+},
+{
+    id: 15,
+    name: "Kim",
+    price: 21.000,
+    category: "Pantalones",
+},
+{
+    id: 16,
+    name: "Hallie",
+    price: 20.000,
+    category: "Pantalones",
+},
+];
